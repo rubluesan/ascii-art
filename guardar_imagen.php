0a55a0c36a13683f4fae27c948b5d57fa1157f18
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2025-11-03 06:33:49 +0100

    enfoque cambiado, usando sesiones

--- guardar_imagen.php
+++ guardar_imagen.php
@@ -1,54 +1,49 @@
 <?php
 
-$rutaImagen = __DIR__ ."/files/".$_FILES['imagen']['name'];
-$info = pathinfo(__DIR__ ."/output/".$_FILES['imagen']['name']);
-$rutaOutSinExtension = $info['dirname'].'/'.$info['filename'];
+// enfoque diferente, usando sesiones, sin guardar archivos
+session_start();
 
-move_uploaded_file($_FILES['imagen']['tmp_name'],  $rutaImagen);
+//imagen que queremos leer (hay que tener gd.lib instalada y activa)
+$img = imagecreatefrompng($_FILES['imagen']['tmp_name']);
 
-//Fichero en el que deseamos guardar el resultado
-    $fp = fopen($rutaOutSinExtension.".txt", "w");
-    
-    //imagen que queremos leer (hay que tener gd.lib instalada y activa)
-    $img = imagecreatefrompng($rutaImagen);
-    
-    //Obtenemos el tamaño
-    $w = imagesx($img); //ancho
-    $h = imagesy($img); //alto
-    $scale = 6;
-    $fixAncho = 2.2; // ancho * 2 ya que los caracteres suelen ser el doble de alto que de ancho
+//Obtenemos el tamaño
+$w = imagesx($img); //ancho
+$h = imagesy($img); //alto
+$scale = 6;
+$fixAncho = 2.2; // ancho * 2 ya que los caracteres suelen ser el doble de alto que de ancho
 
-    for($y = 0; $y < $h; $y+=$scale*$fixAncho) {
-        for($x = 0; $x < $w; $x+=$scale) {
-            $rgb = imagecolorat($img, $x, $y);
+$ascii = '';
 
-            // variables
-            $luminosidad = 0;
-            $caracter = "a";
+for ($y = 0; $y < $h; $y += $scale * $fixAncho) {
+    for ($x = 0; $x < $w; $x += $scale) {
+        $rgb = imagecolorat($img, $x, $y);
 
-            //Valor de las componentes RGB de cada pixel
-            $r = $rgb >> 16;
-            $g = $rgb >> 8 & 255;
-            $b = $rgb & 255;
+        // variables
+        $luminosidad = 0;
+        $caracter = "a";
 
-            $luminosidad = 0.299*$r+0.587*$g+0.114*$b;
+        //Valor de las componentes RGB de cada pixel
+        $r = $rgb >> 16;
+        $g = $rgb >> 8 & 255;
+        $b = $rgb & 255;
 
-            //Elegir el caracter según la luminosidad del pixel y escribir en el fichero
-            if ($luminosidad <= 60) {
-                $caracter = " ";
-            }elseif ($luminosidad <= 120) {
-                $caracter = ".";
-            }elseif ($luminosidad <= 180) {
-                $caracter = "o";
-            }else{
-                $caracter = "@";
-            }
-            
-            fwrite($fp, $caracter);
-        }
-        fwrite($fp, "\n");
+        $luminosidad = 0.299 * $r + 0.587 * $g + 0.114 * $b;
+
+        //Elegir el caracter según la luminosidad del pixel y escribir en el fichero
+        if ($luminosidad <= 60) $caracter = " ";
+        elseif ($luminosidad <= 120) $caracter = ".";
+        elseif ($luminosidad <= 180) $caracter = "o";
+        else $caracter = "@";
+
+        $ascii .= $caracter;
     }
+    $ascii .= "\n";
+}
 
-    fclose($fp);
+// destruimos la imagen
+imagedestroy($img);
 
-    echo "<a href='descargar_ascii.php?file=".$rutaOutSinExtension.".txt'> Descargar Archivo </a>";
+// Guardamos el resultado en la sesión
+$_SESSION['ascii_result'] = $ascii;
+
+echo "<a href='descargar_ascii.php'> Descargar Archivo </a>";
